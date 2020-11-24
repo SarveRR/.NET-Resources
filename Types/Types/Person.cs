19f8add5087a70d2d834f13c914fd20be1911eea
Robert Poręba <webberr14@gmail.com>
2020-11-24 17:26:41 +0100

    Types: Add properties to class

--- Types/Types/Person.cs
+++ Types/Types/Person.cs
@@ -6,5 +6,12 @@ namespace Types
 {
     class Person
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int PhoneNumber { get; set; }
+        public float Weight { get; set; }
+        public string Email { get; set; }
     }
 }
