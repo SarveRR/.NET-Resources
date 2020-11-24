a2c4e97bb8fb7d872a952dacc22a0001bdbb6b61
Robert Poręba <webberr14@gmail.com>
2020-11-24 17:16:41 +0100

    Types: rectangle diagonal

--- Types/Types/Program.cs
+++ Types/Types/Program.cs
@@ -6,24 +6,44 @@ namespace Types
     {
         static void Main(string[] args)
         {
-            string Name = "Name";
-            string Surname = "Surname";
-            int Age = 20;
-            long Pesel = 12345678901;
-            long Number = 3333333333;
+            //1
+            string name = "Name";
+            string surname = "Surname";
+            int age = 20;
+            long pesel = 12345678901;
+            long number = 3333333333;
 
-            Console.WriteLine(Name);
-            Console.WriteLine(Surname);
-            Console.WriteLine(Age);
+            Console.WriteLine(name);
+            Console.WriteLine(surname);
+            Console.WriteLine(age);
             Console.WriteLine(Sex.m);
-            Console.WriteLine(Pesel);
-            Console.WriteLine(Number);
+            Console.WriteLine(pesel);
+            Console.WriteLine(number);
 
+            //2
             char first = 'a';
             char second = 'b';
             char third = 'c';
 
             Console.WriteLine($"{ third }{ second }{ first }");
+
+            //3
+            Console.WriteLine("height: ");
+            string height = Console.ReadLine();
+            Console.WriteLine("width: ");
+            string width = Console.ReadLine();
+
+            double parsedHeight, parsedWidth;
+
+            double.TryParse(height, out parsedHeight);
+            double.TryParse(width, out parsedWidth);
+
+            parsedHeight = Math.Pow(parsedHeight, 2);
+            parsedWidth = Math.Pow(parsedWidth, 2);
+
+            double diagonal = Math.Sqrt(parsedHeight + parsedWidth);
+
+            Console.WriteLine("diagonal = " + diagonal);
         }
 
         enum Sex
