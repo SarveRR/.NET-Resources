c96216b0831e1911ce85863d64e38c7e15016f16
Robert Poręba <webberr14@gmail.com>
2020-11-24 16:55:36 +0100

    Add basic types

--- Types/Types/Program.cs
+++ Types/Types/Program.cs
@@ -6,7 +6,25 @@ namespace Types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Name = "Name";
+            string Surname = "Surname";
+            int Age = 20;
+            long Pesel = 12345678901;
+            long Number = 3333333333;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Surname);
+            Console.WriteLine(Age);
+            Console.WriteLine(Sex.m);
+            Console.WriteLine(Pesel);
+            Console.WriteLine(Number);
         }
+
+        enum Sex
+        {
+            m,
+            k
+        }
+        
     }
 }
