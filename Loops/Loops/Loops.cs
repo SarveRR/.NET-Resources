37cef9f6264a2060771d831a266e992bb1396d8c
Robert Poręba <webberr14@gmail.com>
2020-11-25 16:47:12 +0100

    Loops: Add PrimeNumbers method in Loops class

--- Loops/Loops/Loops.cs
+++ Loops/Loops/Loops.cs
@@ -6,5 +6,19 @@ namespace Loops
 {
     class Loops
     {
+        //1
+        public bool PrimeNumbers(int a)
+        {
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        
     }
 }
--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -6,7 +6,7 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
