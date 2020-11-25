bd3578cdd79f9dab1fb916292954643c6dfbbd9c
Robert Poręba <webberr14@gmail.com>
2020-11-25 12:43:57 +0100

    Conditions: Add ShowDay method to Compare class

--- Conditions/Conditions/Compare.cs
+++ Conditions/Conditions/Compare.cs
@@ -203,5 +203,37 @@ namespace Conditions
                     break;
             }
         }
+
+        //12
+        public void ShowDay(int day)
+        {
+            switch(day)
+            {   
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("sunday");
+                    break;
+                default:
+                    Console.WriteLine("Something goes wrong");
+                    break;
+            }
+        }
     }
 }
