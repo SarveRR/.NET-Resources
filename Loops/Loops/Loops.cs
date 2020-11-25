f6212d46e0112470bba03b9173c94ef0e0ac698d
Robert Poręba <webberr14@gmail.com>
2020-11-25 17:43:27 +0100

    Loops: Add Stars method to Loops class

--- Loops/Loops/Loops.cs
+++ Loops/Loops/Loops.cs
@@ -102,5 +102,27 @@ namespace Loops
                 result = 0;
             }
         }
+
+        //7
+        public void Stars(int number)
+        {
+            for (int i = 0; i <= number; i++)
+            {
+                for (int j = 1; j <= number - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+            for (int i = number - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= number - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
     }
 }
