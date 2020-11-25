bff346404f43a8655a86bfd5ef4d07baae78b004
Robert Poręba <webberr14@gmail.com>
2020-11-25 17:02:38 +0100

    Loops: Add Fibonacci method to Loops class

--- Loops/Loops/Loops.cs
+++ Loops/Loops/Loops.cs
@@ -44,5 +44,14 @@ namespace Loops
             }
             while (i <= 100);
         }
+
+        //3
+        public int Fibonacci(int n)
+        {
+            if ((n == 1) || (n == 2))
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
     }
 }
