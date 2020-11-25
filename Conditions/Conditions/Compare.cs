58d1b2d6af3a321162afdda6cf9798c74a505a49
Robert Poręba <webberr14@gmail.com>
2020-11-25 11:42:28 +0100

    Conditions: Add ComparisonInt method to Compare class

--- Conditions/Conditions/Compare.cs
+++ Conditions/Conditions/Compare.cs
@@ -6,6 +6,19 @@ namespace Conditions
 {
     class Compare
     {
+        public void ComparisonInt()
+        {
+            int first = 5;
+            int second = 5;
 
+            if(first == second)
+            {
+                Console.WriteLine("Same value");
+            }
+            else
+            {
+                Console.WriteLine("Different value");
+            }
+        }
     }
 }
