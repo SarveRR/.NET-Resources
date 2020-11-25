a9baee8e3c2651e1f20be930e2a3b6b46c8e26eb
Robert Poręba <webberr14@gmail.com>
2020-11-25 18:06:00 +0100

    Loops: Add object and all methods to main

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -7,6 +7,45 @@ namespace Loops
         static void Main(string[] args)
         {
             Loops loops = new Loops();
+
+            //1
+            loops.ShowPrimeNumbers();
+            Console.WriteLine("\n");
+
+            //2
+            loops.EvenNumbers();
+            Console.WriteLine("\n");
+
+            //3
+            loops.ShowFibonacci();
+            Console.WriteLine("\n");
+
+            //4
+            loops.PyramidInts(4);
+            Console.WriteLine("\n");
+
+            //5
+            loops.PowerInts();
+            Console.WriteLine("\n");
+
+            //6
+            loops.Addition();
+            Console.WriteLine("\n");
+
+            //7
+            loops.Stars(4);
+            Console.WriteLine("\n");
+
+            //8
+            loops.RiverseOrder(12345);
+            Console.WriteLine("\n");
+
+            //9
+            loops.IntToBinary(20);
+            Console.WriteLine("\n");
+
+            //10
+            loops.LCM(4,5);
         }
     }
 }
