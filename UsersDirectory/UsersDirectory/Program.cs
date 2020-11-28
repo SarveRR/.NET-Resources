63a7582314a7b4e938067a2f4298c28d24b65a43
Robert Poręba <webberr14@gmail.com>
2020-11-28 17:34:58 +0100

    UsersDirectory: Add blank project

---
+++ UsersDirectory/UsersDirectory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsersDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
