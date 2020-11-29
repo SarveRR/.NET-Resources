9791721c401d3df20941b907c095b5580c9b5a31
Robert Poręba <webberr14@gmail.com>
2020-11-29 13:03:12 +0100

    UsersDirectory: Add Initialize method in Program class

--- UsersDirectory/UsersDirectory/Program.cs
+++ UsersDirectory/UsersDirectory/Program.cs
@@ -6,8 +6,20 @@ namespace UsersDirectory
     {
         static void Main(string[] args)
         {
+            MenuActionService actionService = new MenuActionService();
+            Initialize(actionService);
+
             Console.WriteLine("USERS DIRECTORY");
             Console.WriteLine("Choose option:");
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add user", "Main");
+            actionService.AddNewAction(2, "Remove user", "Main");
+            actionService.AddNewAction(3, "Show cities", "Main");
+            actionService.AddNewAction(4, "Show user", "Main");
+            return actionService;
         }
     }
 }
