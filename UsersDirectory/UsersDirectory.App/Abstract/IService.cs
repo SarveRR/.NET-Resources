a1902d4b0bfa2fa2daa09f6d2dc08feb340d296d
Robert Poręba <webberr14@gmail.com>
2020-12-01 18:32:53 +0100

    UsersDirectory: Add IService interface to UsersDirectory.App\Abstract

---
+++ UsersDirectory/UsersDirectory.App/Abstract/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectory.App.Abstract
+{
+    public interface IService<T>
+    {
+        List<T> Users { get; set; }
+        List<T> GetAllUsers();
+        int AddUser(T user);
+        int UpdateUser(T user);
+        void RemoveUser(T user);
+    }
+}
