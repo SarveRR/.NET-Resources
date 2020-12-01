6574790d1b849b1cb80b44e62a411f8e2bc87287
Robert Poręba <webberr14@gmail.com>
2020-12-01 18:40:28 +0100

    UsersDirectory: Add BaseService class UsersDirectory.App\Common

---
+++ UsersDirectory/UsersDirectory.App/Common/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectory.App.Abstract;
+
+namespace UsersDirectory.App.Common
+{
+    public class BaseService<T> : IService<T>
+    {
+        public List<T> Users { get; set; }
+
+        public BaseService()
+        {
+            Users = new List<T>();
+        }
+
+        public int AddUser(T user)
+        {
+            Users.Add(user);
+            return user.Id;
+        }
+
+        public List<T> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(T user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateUser(T user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
