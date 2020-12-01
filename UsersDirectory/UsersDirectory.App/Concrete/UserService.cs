29817778165a6f89d6ed8fce85ca13aade1e752c
Robert Poręba <webberr14@gmail.com>
2020-12-01 19:01:43 +0100

    UsersDirectory: Move MenuActionService class to UsersDirectory.App\Concrete

--- UsersDirectory/UsersDirectory.App/Concrete/UserService.cs
+++ UsersDirectory/UsersDirectory.App/Concrete/UserService.cs
@@ -2,130 +2,77 @@
 using System.Collections.Generic;
 using System.Text;
 using UsersDirectory.App.Common;
+using UsersDirectory.Domain.Entity;
 
 namespace UsersDirectory.App
 {
     public class UserService : BaseService<User>
     {
-        public List<User> Users { get; set; }
-        public UserService()
-        {
-            Users = new List<User>();
-        }
+        //public List<User> Users { get; set; }
+        //public UserService()
+        //{
+        //    Users = new List<User>();
+        //}
 
-        public (int,string,string,string) AddNewUserView(MenuActionService actionService)
-        {
-            Console.WriteLine("Enter new user id:");
-            var id = Console.ReadLine();
-            int userId;
-            Int32.TryParse(id, out userId);
-            Console.WriteLine("Name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Surname:");
-            string surname = Console.ReadLine();
-            Console.WriteLine("City:");
-            string city = Console.ReadLine();
-            Console.Clear();
 
-            return (userId, name, surname, city);
-        }
 
-        public int AddNewUser(int userId, string name, string surName, string city)
-        {
-            User user = new User();
+        //public int UserDetailSelectionView()
+        //{
+        //    Console.WriteLine("Enter user id you want to show:");
+        //    var userId = Console.ReadKey();
+        //    Console.Clear();
+        //    int id;
+        //    Int32.TryParse(userId.KeyChar.ToString(), out id);
 
-            user.Id = userId;
-            user.Name = name;
-            user.SurName = surName;
-            user.City = city;
+        //    return id;
+        //}
 
-            Users.Add(user);
+        //public void UserDetailView(int detailId)
+        //{
+        //    User userToShow = new User();
+        //    foreach (var user in Users)
+        //    {
+        //        if (detailId == user.Id)
+        //        {
+        //            userToShow = user;
+        //            break;
+        //        }
+        //    }
+        //    Console.WriteLine("Id: " + userToShow.Id);
+        //    Console.WriteLine("Name: " + userToShow.Name);
+        //    Console.WriteLine("Surname: " + userToShow.SurName);
+        //    Console.WriteLine("City: " + userToShow.City);
+        //    Console.WriteLine("\nPress any key to back to menu...");
+        //    Console.ReadKey();
+        //    Console.Clear();
+        //}
 
-            return user.Id;
-        }
+        //public string UserByCitySelectionView()
+        //{
+        //    Console.WriteLine("Enter city for users you want to show:");
+        //    var cityName = Console.ReadLine();
+        //    Console.Clear();
 
-        public int RemoveUserView()
-        {
-            Console.WriteLine("Enter user id you want to remove:");
-            var userId = Console.ReadKey();
-            Console.Clear();
-            int id;
-            Int32.TryParse(userId.KeyChar.ToString(), out id);
+        //    return cityName.ToString();
+        //}
 
-            return id;
-        }
-
-        public void RemoveUser(int removeId)
-        {
-            User userToRemove = new User();
-            foreach (var user in Users)
-            {
-                if(removeId == user.Id)
-                {
-                    userToRemove = user;
-                    break;
-                }
-            }
-            Users.Remove(userToRemove);
-        }
-
-        public int UserDetailSelectionView()
-        {
-            Console.WriteLine("Enter user id you want to show:");
-            var userId = Console.ReadKey();
-            Console.Clear();
-            int id;
-            Int32.TryParse(userId.KeyChar.ToString(), out id);
-
-            return id;
-        }
-
-        public void UserDetailView(int detailId)
-        {
-            User userToShow = new User();
-            foreach (var user in Users)
-            {
-                if (detailId == user.Id)
-                {
-                    userToShow = user;
-                    break;
-                }
-            }
-            Console.WriteLine("Id: " + userToShow.Id);
-            Console.WriteLine("Name: " + userToShow.Name);
-            Console.WriteLine("Surname: " + userToShow.SurName);
-            Console.WriteLine("City: " + userToShow.City);
-            Console.WriteLine("\nPress any key to back to menu...");
-            Console.ReadKey();
-            Console.Clear();
-        }
-
-        public string UserByCitySelectionView()
-        {
-            Console.WriteLine("Enter city for users you want to show:");
-            var cityName = Console.ReadLine();
-            Console.Clear();
-
-            return cityName.ToString();
-        }
-
-        public void UserByCityView(string cityName)
-        {
-            List<User> usersToShow = new List<User>();
-            foreach (var user in Users)
-            {
-                if (cityName == user.City)
-                {
-                    usersToShow.Add(user);
-                }
-            }
-            foreach (var user in usersToShow)
-            {
-                Console.WriteLine("Id: " + user.Id+" name: " + user.Name + " surname: " + user.SurName + " city: " + user.City);
-            }
-            Console.WriteLine("\nPress any key to back to menu...");
-            Console.ReadKey();
-            Console.Clear();
-        }
+        //public void UserByCityView(string cityName)
+        //{
+        //    List<User> usersToShow = new List<User>();
+        //    foreach (var user in Users)
+        //    {
+        //        if (cityName == user.City)
+        //        {
+        //            usersToShow.Add(user);
+        //        }
+        //    }
+        //    foreach (var user in usersToShow)
+        //    {
+        //        Console.WriteLine("Id: " + user.Id+" name: " + user.Name + " surname: " + user.SurName + " city: " + user.City);
+        //    }
+        //    Console.WriteLine("\nPress any key to back to menu...");
+        //    Console.ReadKey();
+        //    Console.Clear();
+        //}
     }
 }
--- UsersDirectory/UsersDirectory.Domain/Entity/User.cs
+++ UsersDirectory/UsersDirectory.Domain/Entity/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using UsersDirectory.Domain.Common;
 
-namespace UsersDirectory.Domain
+namespace UsersDirectory.Domain.Entity
 {
     public class User : BaseEntity
     {
