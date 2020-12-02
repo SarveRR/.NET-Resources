52786ee164f0ccb5777341d60b853e08aace7452
Robert Poręba <webberr14@gmail.com>
2020-12-02 17:27:32 +0100

    UserDirectory: Add new GetItemByIdUnitTest

--- UsersDirectory/UsersDirectory.Tests/UnitTests.cs
+++ UsersDirectory/UsersDirectory.Tests/UnitTests.cs
@@ -1,4 +1,10 @@
+using FluentAssertions;
+using Moq;
 using System;
+using UsersDirectory.App.Abstract;
+using UsersDirectory.App.Concrete;
+using UsersDirectory.App.Managers;
+using UsersDirectory.Domain.Entity;
 using Xunit;
 
 namespace UsersDirectory.Tests
@@ -6,9 +12,22 @@ namespace UsersDirectory.Tests
     public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void GetItemByIdUnitTest()
         {
+            //Arrange
+            User user = new User(1, "Name", "Surname", "City");
+            var mock = new Mock<IService<User>>();
+            mock.Setup(u => u.GetUserById(1)).Returns(user);
 
+            var manager = new UserManager(new MenuActionService(), mock.Object);
+
+            //Act
+            var returnedUser = manager.GetUserById(user.Id);
+
+            //Assert
+            returnedUser.Should().BeOfType(typeof(User));
+            returnedUser.Should().NotBeNull();
+            returnedUser.Should().BeSameAs(user);
         }
     }
 }
