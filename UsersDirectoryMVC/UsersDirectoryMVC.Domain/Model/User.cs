ed064f8ce34383f80687b9d69fb6fd79ab1dee28
Robert Poręba <webberr14@gmail.com>
2020-12-11 17:06:46 +0100

    UsersDirectoryMVC: Add User model to .Domain project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Domain.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public int PositionId { get; set; }
+    }
+}
