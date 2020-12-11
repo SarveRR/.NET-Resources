148a438b986186f76c934cd15bdd9e38f5b81bb1
Robert Poręba <webberr14@gmail.com>
2020-12-11 17:18:52 +0100

    UsersDirectoryMVC: Add Employer model to .Domain project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Employer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Domain.Model
+{
+    class Employer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+        public string AddressLine { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
