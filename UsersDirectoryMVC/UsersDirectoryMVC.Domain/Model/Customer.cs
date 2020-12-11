f22138c804f4a5e23d78db876b5d49f35e64d78f
Robert Poręba <webberr14@gmail.com>
2020-12-11 17:48:58 +0100

    UsersDirectoryMVC: Add more properties to Customer model

--- UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Customer.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Customer.cs
@@ -9,6 +9,13 @@ namespace UsersDirectoryMVC.Domain.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string NIP { get; set; }
-        public string AddressLine { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public string FlatNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
