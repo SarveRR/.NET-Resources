ba32f35c00cf5cb9af09a99ea6bd83cc6bb68051
Robert Poręba <webberr14@gmail.com>
2020-12-11 18:48:07 +0100

    UsersDirectoryMVC: Create Context.cs in .Infrastructure project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Domain.Model;
+
+namespace UsersDirectoryMVC.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
