9636904dcc3126d8da495e2a77c8779f1cb9353f
Robert Poręba <webberr14@gmail.com>
2020-12-11 17:15:26 +0100

    UsersDirectoryMVC: Add Customer model to .Domain project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Domain.Model
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+        public string AddressLine { get; set; }
+    }
+}
