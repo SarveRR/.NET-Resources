b4e7540bea502147f1a9a02b4c04d5e04015518c
Robert Poręba <webberr14@gmail.com>
2020-12-11 18:11:56 +0100

    UsersDirectoryMVC: Add Assignment model to .Domain project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Domain.Model
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
