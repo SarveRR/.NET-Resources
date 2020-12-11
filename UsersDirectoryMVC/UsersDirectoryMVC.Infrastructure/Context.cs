9edcb8078dc507127146dabe7ca6357daa49d7d1
Robert Poręba <webberr14@gmail.com>
2020-12-11 18:52:48 +0100

    UsersDirectoryMVC: Add properties to context.cs

--- UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Context.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Context.cs
@@ -9,6 +9,18 @@ namespace UsersDirectoryMVC.Infrastructure
 {
     public class Context : IdentityDbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<AssignmentTag> AssignmentTag { get; set; }
+        public DbSet<ContactDetail> ContactDetails { get; set; }
+        public DbSet<ContactDetailType> ContactDetailTypes { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerContactInformation> CustomerContactInformations { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public Context(DbContextOptions options) : base(options)
         {
         }
