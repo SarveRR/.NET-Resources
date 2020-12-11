584cd931821a00fb42fa4cd78a2d5c0d920c2362
Robert Poręba <webberr14@gmail.com>
2020-12-11 18:04:45 +0100

    UsersDirectoryMVC: Add Tag model to .Domain project

--- UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/CustomerContactInformation.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/CustomerContactInformation.cs
@@ -7,8 +7,8 @@ namespace UsersDirectoryMVC.Domain.Model
     public class CustomerContactInformation
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public int CustomerRef { get; set; }
         public Customer Customer { get; set; }
