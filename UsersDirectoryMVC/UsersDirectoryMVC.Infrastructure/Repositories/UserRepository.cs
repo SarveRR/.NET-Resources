4227b423f1949c70337e9ac03730224f7a51dbb0
Robert Poręba <webberr14@gmail.com>
2020-12-13 11:14:36 +0100

    UsersDirectoryMVC: Add UserRepository to .Infrastructure project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Infrastructure.Repositories
+{
+    public class UserRepository
+    {
+        private readonly Context _context;
+        public UserRepository(Context context)
+        {
+            _context = context;
+        }
+    }
+}
