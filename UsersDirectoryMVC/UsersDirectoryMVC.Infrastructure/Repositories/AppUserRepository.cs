caeedb82de1a45161c4a21f45e63e9848d82c151
Robert Poręba <webberr14@gmail.com>
2020-12-13 11:44:20 +0100

    UsersDirectoryMVC: Add CRUD methods to AppUsersRepository

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/AppUserRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UsersDirectoryMVC.Domain.Model;
+
+namespace UsersDirectoryMVC.Infrastructure.Repositories
+{
+    public class AppUserRepository
+    {
+        private readonly Context _context;
+        public AppUserRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void DeleteAppUser(int appUserId)
+        {
+            var appUser = _context.AppUsers.Find(appUserId);
+            if(appUser != null)
+            {
+                _context.AppUsers.Remove(appUser);
+                _context.SaveChanges();
+            }
+        }
+
+        public int AddAppUser(AppUser appUser)
+        {
+            _context.AppUsers.Add(appUser);
+            _context.SaveChanges();
+            return appUser.Id;
+        }
+
+        public IQueryable<AppUser> GetAppUserByPositionId(int positionId)
+        {
+            var appUsers = _context.AppUsers.Where(a => a.PositionId == positionId);
+            return appUsers;
+        }
+
+        public AppUser GetAppUserById(int appUserId)
+        {
+            var appUser = _context.AppUsers.FirstOrDefault(a => a.Id == appUserId);
+            return appUser;
+        }
+
+        public IQueryable<Tag> GetAllTags()
+        {
+            var tags = _context.Tags;
+            return tags;
+        }
+
+        public IQueryable<AppUser> GetAllAppUsers()
+        {
+            var appUsers = _context.AppUsers;
+            return appUsers;
+        }
+
+        public IQueryable<Position> GetAllPositions()
+        {
+            var positions = _context.Positions;
+            return positions;
+        }
+    }
+}
