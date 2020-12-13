3f8dcee5d48c6e15a91cda9133e91bdd5aa3ab4b
Robert Poręba <webberr14@gmail.com>
2020-12-13 11:59:23 +0100

    UsersDirectoryMVC: Add CRUD methods to EmployerRepository

--- UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/EmployerRepository.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/EmployerRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using UsersDirectoryMVC.Domain.Model;
 
 namespace UsersDirectoryMVC.Infrastructure.Repositories
 {
@@ -11,6 +13,35 @@ namespace UsersDirectoryMVC.Infrastructure.Repositories
         public EmployerRepository(Context context)
         {
             _context = context;
+        }
+
+        public void DeleteEmployer(int employerId)
+        {
+            var employer = _context.Employers.Find(employerId);
+            if (employer != null)
+            {
+                _context.Employers.Remove(employer);
+                _context.SaveChanges();
+            }
+        }
+
+        public int AddEmployer(Employer employer)
+        {
+            _context.Employers.Add(employer);
+            _context.SaveChanges();
+            return employer.Id;
+        }
+
+        public Employer GetEmployerById(int employerId)
+        {
+            var employer = _context.Employers.FirstOrDefault(a => a.Id == employerId);
+            return employer;
+        }
+
+        public IQueryable<Employer> GetAllEmployers()
+        {
+            var employers = _context.Employers;
+            return employers;
         }
     }
 }
