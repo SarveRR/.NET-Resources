8f914aeb5007cd28f1adfc01becf5f13c5f3074c
Robert Poręba <webberr14@gmail.com>
2020-12-13 11:48:51 +0100

    UsersDirectoryMVC: Add EmployerRepository to .Infrastructure project

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/EmployerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Infrastructure.Repositories
+{
+    public class EmployerRepository
+    {
+        private readonly Context _context;
+
+        public EmployerRepository(Context context)
+        {
+            _context = context;
+        }
+    }
+}
