a7e80229646323d4aa2c58177bee0c8646a96383
Robert Poręba <webberr14@gmail.com>
2020-12-14 14:29:29 +0100

    UsersDirectoryMVC: Add CustomerRepository to .Infrastructure

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Infrastructure.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly Context _context;
+
+        public CustomerRepository(Context context)
+        {
+            _context = context;
+        }
+    }
+}
