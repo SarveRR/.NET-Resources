479f44e60a96b7db6fb395e9d95a58d5a3209df9
Robert Poręba <webberr14@gmail.com>
2020-12-14 14:34:37 +0100

    UsersDirectoryMVC: Add AssignmentRepository to .Infrastructure

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/AssignmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Infrastructure.Repositories
+{
+    public class AssignmentRepository
+    {
+        private readonly Context _context;
+
+        public AssignmentRepository(Context context)
+        {
+            _context = context;
+        }
+    }
+}
