105706dd7e1bd9b07dc6bf6f740e44bd9b80e4ab
Robert Poręba <webberr14@gmail.com>
2020-12-14 14:37:27 +0100

    UsersDirectoryMVC: Add CRUD methods to AssignmentRepository

--- UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/AssignmentRepository.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Infrastructure/Repositories/AssignmentRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using UsersDirectoryMVC.Domain.Model;
 
 namespace UsersDirectoryMVC.Infrastructure.Repositories
 {
@@ -11,6 +13,35 @@ namespace UsersDirectoryMVC.Infrastructure.Repositories
         public AssignmentRepository(Context context)
         {
             _context = context;
+        }
+
+        public void DeleteAssignment(int assignmentId)
+        {
+            var assignment = _context.Assignments.Find(assignmentId);
+            if (assignment != null)
+            {
+                _context.Assignments.Remove(assignment);
+                _context.SaveChanges();
+            }
+        }
+
+        public int AddAssignment(Assignment assignment)
+        {
+            _context.Assignments.Add(assignment);
+            _context.SaveChanges();
+            return assignment.Id;
+        }
+
+        public Assignment GetAssignmentById(int assignmentId)
+        {
+            var assignment = _context.Assignments.FirstOrDefault(a => a.Id == assignmentId);
+            return assignment;
+        }
+
+        public IQueryable<Assignment> GetAllEmployers()
+        {
+            var assignments = _context.Assignments;
+            return assignments;
         }
     }
 }
