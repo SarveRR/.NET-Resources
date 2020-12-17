cb31c57648226782ff138735c708a4ea3343b664
Robert Poręba <webberr14@gmail.com>
2020-12-17 14:35:56 +0100

    UsersDirectoryMVC: Add NewCustomerVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/NewCustomerVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Customer
+{
+    public class NewCustomerVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+    }
+}
