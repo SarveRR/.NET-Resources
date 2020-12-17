9a8225f2d69f284828bcec79d82fd3ada5958e47
Robert Poręba <webberr14@gmail.com>
2020-12-17 14:32:19 +0100

    UsersDirectoryMVC: Add CustomerDetailsVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerDetailsVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Customer
+{
+    public class CustomerDetailsVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+        public string CEOFullName { get; set; }
+        public string FirstLineOfContactInformation { get; set; }
+        public List<AddressForListVm> Addresses { get; set; }
+        public List<ContactDetailListVm> Email { get; set; }
+        public List<ContactDetailListVm> PhoneNumber { get; set; }
+    }
+}
