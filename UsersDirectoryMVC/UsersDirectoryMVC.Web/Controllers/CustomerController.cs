44e60f74a6d6c5788bca6a7df7500da7b64dd33f
Robert Poręba <webberr14@gmail.com>
2020-12-17 14:10:16 +0100

    UsersDirectoryMVC: Add CustomerController to .Web

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Web/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersDirectoryMVC.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = customerService.GetAllCustomersForList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCustomer(CustomerModel model)
+        {
+            var id = customerService.AddCustomer(model);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNewAddressForClient(int customerId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewAddressForClient(AddressModel model)
+        {
+            return View();
+        }
+
+        public IActionResult ViewCustomer(int customerId)
+        {
+            var customerModel = customerService.GetCustomerById(customerId);
+            return View(customerModel);
+        }
+    }
+}
