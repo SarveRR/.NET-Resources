32c44d388467776940247814ae207df145f34550
Robert Poręba <webberr14@gmail.com>
2020-12-17 14:23:07 +0100

    UsersDirectoryMVC: Add CustomerFoListVm to .Appllication

--- UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Customer.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Domain/Model/Customer.cs
@@ -9,6 +9,8 @@ namespace UsersDirectoryMVC.Domain.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string NIP { get; set; }
+        public string CEOFisrtName { get; set; }
+        public string CEOLastName { get; set; }
 
         public CustomerContactInformation CustomerContactInformation { get; set; }
         public virtual ICollection<ContactDetail> ContactDetails { get; set; }
