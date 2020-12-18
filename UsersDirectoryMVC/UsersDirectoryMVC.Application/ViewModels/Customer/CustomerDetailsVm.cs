1de3cfd55bb81fbb499107cbe0f01106f022a1b4
Robert Poręba <webberr14@gmail.com>
2020-12-18 13:21:53 +0100

    UsersDirectoryMVC: Add mapping to CustomerDetailsVm from .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerDetailsVm.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerDetailsVm.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
 
 namespace UsersDirectoryMVC.Application.ViewModels.Customer
 {
-    public class CustomerDetailsVm
+    public class CustomerDetailsVm : IMapFrom<UsersDirectoryMVC.Domain.Model.Customer>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -14,5 +16,16 @@ namespace UsersDirectoryMVC.Application.ViewModels.Customer
         public List<AddressForListVm> Addresses { get; set; }
         public List<ContactDetailListVm> Emails { get; set; }
         public List<ContactDetailListVm> PhoneNumbers { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.Customer, CustomerDetailsVm>()
+                .ForMember(s => s.CEOFullName, opt => opt.MapFrom(d => d.CEOFisrtName + " " + d.CEOLastName))
+                .ForMember(s => s.FirstLineOfContactInformation, opt => opt.MapFrom(d => d.CustomerContactInformation.FirstName
+                + " " + d.CustomerContactInformation.LastName))
+                .ForMember(s => s.Addresses, opt => opt.Ignore())
+                .ForMember(s => s.Emails, opt => opt.Ignore())
+                .ForMember(s => s.PhoneNumbers, opt => opt.Ignore());
+        }
     }
 }
