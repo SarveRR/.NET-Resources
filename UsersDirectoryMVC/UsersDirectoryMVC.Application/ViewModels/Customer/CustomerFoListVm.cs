28dc8b8d45c5e06212da735feced6b6a299f2adc
Robert Poręba <webberr14@gmail.com>
2020-12-18 13:13:56 +0100

    UsersDirectoryMVC: Add mapping to CustomerForListVm from .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerFoListVm.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerFoListVm.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
 
 namespace UsersDirectoryMVC.Application.ViewModels.Customer
 {
-    public class CustomerForListVm
+    public class CustomerForListVm : IMapFrom<UsersDirectoryMVC.Domain.Model.Customer>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string NIP { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.Customer, CustomerForListVm>();
+        }
     }
 }
