4c8ddbc42d06fe5f64a541bb57eb7d594a487795
Robert Poręba <webberr14@gmail.com>
2020-12-23 13:32:15 +0100

    UsersDirectoryMVC: Add EmployerFoListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Employer/EmployerFoListVm.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Employer
+{
+    public class EmployerFoListVm : IMapFrom<UsersDirectoryMVC.Domain.Model.Employer>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.Employer, EmployerFoListVm>();
+        }
+    }
+}
