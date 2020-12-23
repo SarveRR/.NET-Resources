ae80c97047970c28e4006b848077330ce202fc91
Robert Poręba <webberr14@gmail.com>
2020-12-23 16:46:52 +0100

    UsersDirectoryMVC: Add AppUserForListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AppUser/AppUserForListVm.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.AppUser
+{
+    public class AppUserForListVm : IMapFrom<UsersDirectoryMVC.Domain.Model.AppUser>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.AppUser, AppUserForListVm>();
+        }
+    }
+}
