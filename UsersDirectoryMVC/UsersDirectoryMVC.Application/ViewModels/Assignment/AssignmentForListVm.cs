362578db0d9242c68c4588a83f07b5251d05fa62
Robert Poręba <webberr14@gmail.com>
2020-12-23 15:26:15 +0100

    UsersDirectoryMVC: Add AssignmentForListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Assignment/AssignmentForListVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Assignment
+{
+    public class AssignmentForListVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.Employer, AssignmentForListVm>();
+        }
+    }
+}
