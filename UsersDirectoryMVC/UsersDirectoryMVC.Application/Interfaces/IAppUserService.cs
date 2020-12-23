c21ab90fef2e2ed79cbfda57337af8596558b034
Robert Poręba <webberr14@gmail.com>
2020-12-23 16:51:02 +0100

    UsersDirectoryMVC: Add IAppUserService to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAppUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.ViewModels.AppUser;
+
+namespace UsersDirectoryMVC.Application.Interfaces
+{
+    public interface IAppUserService
+    {
+        ListAppUserForListVm GetAllActiveAppUsersForList(int pageSize, int pageNumber, string searchString);
+        int AddAppUser(NewAppUserVm appUser);
+        AppUserDetailsVm GetAppUserDetails(int id);
+        NewAppUserVm GetAppUserForEdit(int id);
+        void UpdateAppUser(NewAppUserVm model);
+        void DeleteAppUser(int id);
+    }
+}
