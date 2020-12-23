50a46c0478612cf675e24db959c1aeb0cdfc1cce
Robert Poręba <webberr14@gmail.com>
2020-12-23 16:49:05 +0100

    UsersDirectoryMVC: Add AppUserDetailsVm to .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AppUser/AppUserDetailsVm.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AppUser/AppUserDetailsVm.cs
@@ -6,15 +6,16 @@ using UsersDirectoryMVC.Application.Mapping;
 
 namespace UsersDirectoryMVC.Application.ViewModels.AppUser
 {
-    public class AppUserForListVm : IMapFrom<UsersDirectoryMVC.Domain.Model.AppUser>
+    public class AppUserDetailsVm : IMapFrom<UsersDirectoryMVC.Domain.Model.AppUser>
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string City { get; set; }
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<UsersDirectoryMVC.Domain.Model.AppUser, AppUserForListVm>();
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.AppUser, AppUserDetailsVm>();
         }
     }
 }
