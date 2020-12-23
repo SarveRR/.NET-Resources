2dc48eb5736f5066e826715a6bbfabc2fbae8911
Robert Poręba <webberr14@gmail.com>
2020-12-23 13:34:37 +0100

    UsersDirectoryMVC: Add EmployerDetailsVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Employer/EmployerDetailsVm.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Employer
+{
+    public class EmployerDetailsVm : IMapFrom<UsersDirectoryMVC.Domain.Model.Employer>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NIP { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<EmployerDetailsVm, UsersDirectoryMVC.Domain.Model.Employer>();
+        }
+    }
+}
