064eb06142fcdd8b22b43017a242c4d835070735
Robert Poręba <webberr14@gmail.com>
2020-12-23 15:25:02 +0100

    UsersDirectoryMVC: Add ListAssignmentForListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Assignment/ListAssignmentForListVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Assignment
+{
+    public class ListAssignmentForListVm
+    {
+        public List<AssignmentForListVm> Assignments { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public string SearchString { get; set; }
+        public int Count { get; set; }
+    }
+}
