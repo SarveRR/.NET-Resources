e6075668fe388f21a2bd67e7252a4843dc0ac2ab
Robert Poręba <webberr14@gmail.com>
2020-12-23 15:29:07 +0100

    UsersDirectoryMVC: Add AssignmentDetailsVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Assignment/AssignmentDetailsVm.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.ViewModels.Employer;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Assignment
+{
+    public class AssignmentDetailsVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.Employer, AssignmentDetailsVm>();
+        }
+    }
+}
