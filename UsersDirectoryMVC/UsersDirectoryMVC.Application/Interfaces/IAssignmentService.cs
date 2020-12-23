c27b798fcc0cf932ca82610344c64df020d58a4d
Robert Poręba <webberr14@gmail.com>
2020-12-23 15:23:53 +0100

    UsersDirectoryMVC: Add NewAssignmentVm to .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAssignmentService.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAssignmentService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UsersDirectoryMVC.Application.ViewModels.Assignment;
 
 namespace UsersDirectoryMVC.Application.Interfaces
 {
---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Assignment/NewAssignmentVm.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Assignment
+{
+    public class NewAssignmentVm : IMapFrom<UsersDirectoryMVC.Domain.Model.Assignment>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<NewAssignmentVm, UsersDirectoryMVC.Domain.Model.Assignment>().ReverseMap();
+        }
+    }
+
+    public class NewAssignmentValidation : AbstractValidator<NewAssignmentVm>
+    {
+        public NewAssignmentValidation()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).MaximumLength(255);
+            RuleFor(x => x.Description).MaximumLength(255);
+        }
+    }
+}
