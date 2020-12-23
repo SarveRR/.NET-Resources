d4f27f5db52d14ac5688b38c4466e01842e1e423
Robert Poręba <webberr14@gmail.com>
2020-12-23 13:57:37 +0100

    UsersDirectoryMVC: Add Employer to DependencyInjection to .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/DependencyInjection.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/DependencyInjection.cs
@@ -8,6 +8,7 @@ using System.Text;
 using UsersDirectoryMVC.Application.Interfaces;
 using UsersDirectoryMVC.Application.Services;
 using UsersDirectoryMVC.Application.ViewModels.Customer;
+using UsersDirectoryMVC.Application.ViewModels.Employer;
 
 namespace UsersDirectoryMVC.Application
 {
@@ -16,9 +17,11 @@ namespace UsersDirectoryMVC.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<IEmployerService, EmployerService>();
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+ 
             services.AddTransient<IValidator<NewCustomerVm>, NewCustomerValidation>();
+            services.AddTransient<IValidator<NewEmployerVm>, NewEmployerValidation>();
             return services;
         }
     }
