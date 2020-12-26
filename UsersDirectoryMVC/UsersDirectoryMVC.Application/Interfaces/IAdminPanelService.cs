b0c698a46f19461dc930e13196309d36a3a02b13
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:53:18 +0100

    UsersDirectoryMVC: Add AdminPanelService to .Application

--- UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAdminPanelService.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAdminPanelService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UsersDirectoryMVC.Application.ViewModels.AdminPanel;
 
 namespace UsersDirectoryMVC.Application.Interfaces
 {
---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/Services/AdminPanelService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UsersDirectoryMVC.Application.Interfaces;
+using UsersDirectoryMVC.Application.ViewModels.AdminPanel;
+
+namespace UsersDirectoryMVC.Application.Services
+{
+    public class AdminPanelService : IAdminPanelService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+        public AdminPanelService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        public ListUsersForListVm GetAllUsers()
+        {
+            var users = _userManager.Users.ProjectTo<UserForListVm>(_mapper.ConfigurationProvider).ToList();
+            var usersVm = new ListUsersForListVm()
+            {
+                Users = users,
+                Count = users.Count
+            };
+            return usersVm;
+        }
+    }
+}
