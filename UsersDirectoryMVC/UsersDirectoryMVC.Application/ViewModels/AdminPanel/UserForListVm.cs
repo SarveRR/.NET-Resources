057a7c5e7ab5a42def57baf872db4d7813941263
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:49:20 +0100

    UsersDirectoryMVC: Add UserForListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AdminPanel/UserForListVm.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.AdminPanel
+{
+    public class UserForListVm : IMapFrom<IdentityUser>
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<IdentityUser, UserForListVm>();
+        }
+    }
+}
