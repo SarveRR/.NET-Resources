dd1b2d42b304aa9e47a408e20cc3fd985dc145a9
Robert Poręba <webberr14@gmail.com>
2020-12-26 21:12:29 +0100

    Revert "UsersDirectoryMVC: Add Facebook Register and Login"
    This reverts commit 405f4bcec4a93b388b6cf458f97d7e91dcd0c126.

--- UsersDirectoryMVC/UsersDirectoryMVC.Web/Startup.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Web/Startup.cs
@@ -57,18 +57,12 @@ namespace UsersDirectoryMVC.Web
                 options.User.RequireUniqueEmail = true;
             });
 
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
-                    options.ClientId = googleAuthNSection["ClientId"];
-                    options.ClientSecret = googleAuthNSection["ClientSecret"];
-                })
-                .AddFacebook(options =>
-                {
-                    options.AppId = Configuration["Authentication:Facebook:AppId"];
-                    options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-                });
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
