fd90dfbb2a88fc553f62fb1005b474d4107ac82b
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:45:04 +0100

    UsersDirectoryMVC: Add AdminPanelController to .Web

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Web/Controllers/AdminPanelController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersDirectoryMVC.Web.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private readonly IAdminPanelService _adminPanelService;
+        public AdminPanelController(IAdminPanelService adminPanelService)
+        {
+            _adminPanelService = adminPanelService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _adminPanelService.GetAllUsers();
+            return View(model);
+        }
+    }
+}
