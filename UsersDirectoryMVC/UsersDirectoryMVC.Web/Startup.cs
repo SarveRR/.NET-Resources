95ea556d820d49abcccf6d9a89e0457e63018ec1
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:16:21 +0100

    Revert "UsersDirectoryMVC: Add Google Register and Login"
    This reverts commit dd50479c65dc690de4559f59b57c8867b8507141.

--- UsersDirectoryMVC/UsersDirectoryMVC.Web/Startup.cs
+++ UsersDirectoryMVC/UsersDirectoryMVC.Web/Startup.cs
@@ -56,13 +56,6 @@ namespace UsersDirectoryMVC.Web
                 options.SignIn.RequireConfirmedEmail = false;
                 options.User.RequireUniqueEmail = true;
             });
-
-            services.AddAuthentication().AddGoogle(options =>
-            {
-                IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
-                options.ClientId = googleAuthNSection["ClientId"];
-                options.ClientSecret = googleAuthNSection["ClientSecret"];
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
