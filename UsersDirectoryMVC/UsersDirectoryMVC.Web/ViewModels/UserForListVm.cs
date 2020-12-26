47576154e6940fb58ec4713f882dff6d7c6bf4ac
Robert Poręba <webberr14@gmail.com>
2020-12-26 21:12:29 +0100

    Revert "UsersDirectoryMVC: Add Facebook Register and Login"
    This reverts commit 405f4bcec4a93b388b6cf458f97d7e91dcd0c126.

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Web/ViewModels/UserForListVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersDirectoryMVC.Web.ViewModels
+{
+    public class UserForListVm
+    {
+        public char Id { get; set; }
+        public char UserName { get; set; }
+    }
+}
