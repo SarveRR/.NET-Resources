d708813f4d7b822fbd44428b634e457531a358ce
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:46:34 +0100

    UsersDirectoryMVC: Add IAdminPanelService to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/Interfaces/IAdminPanelService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.Interfaces
+{
+    public interface IAdminPanelService
+    {
+        ListUsersForListVm GetAllUsers();
+    }
+}
