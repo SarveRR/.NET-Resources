d67b0646b0f36fade8ac4292d5e166934bec3189
Robert Poręba <webberr14@gmail.com>
2020-12-26 23:27:13 +0100

    UsersDirectoryMVC: Add RoleVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AdminPanel/RoleVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.AdminPanel
+{
+    public class RoleVm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<IdentityRole, RoleVm>();
+        }
+    }
+}
