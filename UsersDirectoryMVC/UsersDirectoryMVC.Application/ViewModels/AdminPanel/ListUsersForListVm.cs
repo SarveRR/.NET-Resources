5e6bbac752eb9d7e25b4b5ad70f5d904071a5c96
Robert Poręba <webberr14@gmail.com>
2020-12-26 22:48:01 +0100

    UsersDirectoryMVC: Add ListUsersForListVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/AdminPanel/ListUsersForListVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersDirectoryMVC.Application.ViewModels.AdminPanel
+{
+    public class ListUsersForListVm
+    {
+        public List<UserForListVm> Users { get; set; }
+        public int Count { get; set; }
+    }
+}
