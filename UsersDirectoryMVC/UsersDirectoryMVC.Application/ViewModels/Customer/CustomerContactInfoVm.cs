b614ee10d11d403b2518ed425a07a0d23a36e0ca
Robert Poręba <webberr14@gmail.com>
2020-12-30 13:08:04 +0100

    UsersDirectoryMVC: Add CustomerContactInfoVm to .Application

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Application/ViewModels/Customer/CustomerContactInfoVm.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsersDirectoryMVC.Application.Mapping;
+
+namespace UsersDirectoryMVC.Application.ViewModels.Customer
+{
+    public class CustomerContactInfoVm : IMapFrom<UsersDirectoryMVC.Domain.Model.CustomerContactInformation>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UsersDirectoryMVC.Domain.Model.CustomerContactInformation, CustomerContactInfoVm>().ReverseMap();
+        }
+    }
+}
