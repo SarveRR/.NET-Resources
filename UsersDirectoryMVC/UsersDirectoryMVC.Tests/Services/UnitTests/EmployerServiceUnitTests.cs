9da4a900d1b0fd859c92c8902f5160b44c1d1a31
Robert Poręba <webberr14@gmail.com>
2021-01-04 22:14:39 +0100

    UsersDirectoryMVC: Add EmployerServiceUnitTests to .Tests

---
+++ UsersDirectoryMVC/UsersDirectoryMVC.Tests/Services/UnitTests/EmployerServiceUnitTests.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using UsersDirectoryMVC.Application.Mapping;
+using UsersDirectoryMVC.Application.Services;
+using UsersDirectoryMVC.Application.ViewModels.AppUser;
+using UsersDirectoryMVC.Application.ViewModels.Employer;
+using UsersDirectoryMVC.Domain.Interfaces;
+using UsersDirectoryMVC.Domain.Model;
+using Xunit;
+
+namespace UsersDirectoryMVC.Tests
+{
+    public class EmployerServiceUnitTests
+    {
+        [Fact]
+        public void CheckEmployerIdAfterAdd()
+        {
+            //Arrange
+            NewEmployerVm employerToAdd = new NewEmployerVm()
+            {
+                Id = 6,
+                Name = "test",
+                NIP = "Unit"
+            };
+
+            Employer employer = new Employer()
+            {
+                Id = 6,
+                Name = "test",
+                NIP = "Unit"
+            };
+
+            var config = new MapperConfiguration(c =>
+            {
+                c.AddProfile(new MappingProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            var mock = new Mock<IEmployerRepository>();
+            mock.Setup(s => s.AddEmployer(employer)).Returns(employer.Id);
+
+            var manager = new EmployerService(mapper, mock.Object);
+
+            //Act
+            var result = manager.AddEmployer(employerToAdd);
+
+            //Assert
+            result.Should().Equals(employer.Id);
+        }
+
+        [Fact]
+        public void CheckEmployerDetails()
+        {
+            //Arrange
+            Employer employer = new Employer()
+            {
+                Id = 6,
+                Name = "test",
+                NIP = "unit",
+            };
+
+            var config = new MapperConfiguration(c =>
+            {
+                c.AddProfile(new MappingProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            var mock = new Mock<IEmployerRepository>();
+            mock.Setup(s => s.GetEmployer(6)).Returns(employer);
+
+            var manager = new EmployerService(mapper, mock.Object);
+
+            //Act
+            var result = manager.GetEmployerDetails(6);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Id.Should().Equals(employer.Id);
+            employer.Id = 7;
+            result.Id.Should().Equals(employer.Id);
+            result.Name.Should().Equals(employer.Name);
+            result.NIP.Should().Equals(employer.NIP);
+        }
+
+        [Fact]
+        public void CheckEmployerToEditDetails()
+        {
+            //Arrange
+            Employer employer = new Employer()
+            {
+                Id = 6,
+                Name = "test",
+                NIP = "unit",
+            };
+
+            var config = new MapperConfiguration(c =>
+            {
+                c.AddProfile(new MappingProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            var mock = new Mock<IEmployerRepository>();
+            mock.Setup(s => s.GetEmployer(6)).Returns(employer);
+
+            var manager = new EmployerService(mapper, mock.Object);
+
+            //Act
+            var result = manager.GetEmployerForEdit(6);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Id.Should().Equals(employer.Id);
+            employer.Id = 7;
+            result.Id.Should().Equals(employer.Id);
+            result.Name.Should().Equals(employer.Name);
+            result.NIP.Should().Equals(employer.NIP);
+        }
+    }
+}
